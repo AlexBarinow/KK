5758e49e9aef9d890dad5403bbe981f04b4f24a0
alex <username.alex64@gmail.com>
2022-05-03 16:22:00 +0300

    iCommit

--- src/Task2/Task2.java
+++ src/Task2/Task2.java
@@ -1,7 +1,23 @@
+//2) Написать программу, которая будет просить у пользователя ввести одномерный массив целочисленных значений,
+// требуется вывести сумму этих элементов.
+
 package Task2;
-//2) Написать программу, которая будет просить у пользователя ввести одномерный массив целочисленных значений, требуется вывести сумму этих элементов.
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class Task2 {
 
+    public static void main(String[] args) {
+        int[] array = new int[5];                       //array created
+        Scanner in = new Scanner(System.in);
 
+        for (int i = 0; i < array.length; i++)          //filling array
+            array[i] = in.nextInt();
 
+        System.out.println(IntStream.of(array).sum());  //output the sum
+
+    }
 }
+
