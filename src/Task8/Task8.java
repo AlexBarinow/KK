79a8b25f978b8a68e24be3780c75c40dbe953851
alex <username.alex64@gmail.com>
2022-05-04 01:16:00 +0300

    commit

--- src/Task8/Task8.java
+++ src/Task8/Task8.java
@@ -1,18 +1,34 @@
+//8) Написать программу, которая будет
+// заполнять нулями матрицу так, чтобы она стала треугольной. Вывести её на экран.
 package Task8;
 
 import java.util.Scanner;
 
 public class Task8 {
 
-    static Scanner in = new Scanner(System.in);
+    static int size = 3;
+    static int[][] array = new int[size][size];
+    static int bound1 = 1;
+    static int bound2 = 10;
+
+
 
     public static void main(String[] args) {
 
+        assignArrayRandom(array,bound1, bound2);
+
+
+
+
     }
 
-    private static void assignArray(double[][] arr) {
+    public static int getRandomInBoundsof(int a, int b) {
+        return a + (int) (Math.random() * b);
+    }
+
+    private static void assignArrayRandom(int[][] arr, int b1, int b2) {
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr[i].length; j++)
-                arr[i][j] = in.nextDouble();
+                arr[i][j] = getRandomInBoundsof(b1, b2);
     }
 }
